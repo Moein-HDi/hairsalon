8f7792bb6f2b4741c67193bc414867644a5c01e0
MoeinHDi <moein.hedayati@gmail.com>
2022-09-27 12:07:14 +0330

    Add project files.

---
+++ hairsalon/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hairsalon.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult login()
+        {
+            ViewBag.Message = "صفحه ورود";
+
+            return View();
+        }
+    }
+}
---
+++ hairsalon/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hairsalon.MvcApplication" Language="C#" %>
