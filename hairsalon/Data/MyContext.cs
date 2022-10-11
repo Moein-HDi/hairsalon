550956bb26e80ee10224b79f1ca583f7c7b9883a
MoeinHDi <moein.hedayati@gmail.com>
2022-10-11 23:06:14 +0330

    adding db models

---
+++ hairsalon/Data/MyContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace hairsalon.Data
+{
+    public class MyContext : DbContext
+    {
+        public MyContext() : base("name=MyConnectionString")
+        {
+
+        }
+        public virtual DbSet<Account> Accounts { get; set; }
+    }
+}
---
+++ hairsalon/Models/CheckPhoneVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace hairsalon.Models
+{
+    public class CheckPhoneVM
+    {
+        public int PhoneNumber { get; set; }
+
+    }
+}
